33a73a9db8b99beb7c5170b8da50b48d56734ee6
Konrad Pawelec <konrad.pawelec@icloud.com>
2023-09-04 13:33:07 +0200

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      "roboto-mono": ['Roboto Mono', "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
